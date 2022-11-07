68aa30f76abdd04f8152a4b8abebf5b12cd7e63b
Filip Antonsson <fian12@student.bth.se>
2022-11-07 17:51:06 +0000

    v11.1.0

--- app/models.py
+++ app/models.py
@@ -33,18 +33,30 @@ class User(UserMixin, db.Model):
         backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
 
     def follow(self, user):
+	"""
+	Check if main following, add user.
+	"""
         if not self.is_following(user):
             self.followed.append(user)
 
     def unfollow(self, user):
+	"""
+	Check if main following, remove user.
+	"""
         if self.is_following(user):
             self.followed.remove(user)
 
     def is_following(self, user):
+	"""
+	Check if main is following user.
+	"""
         return self.followed.filter(
             followers.c.followed_id == user.id).count() > 0
 
     def followed_posts(self):
+	"""
+	Check all follower posts.
+	"""
         followed = Post.query.join(
             followers, (followers.c.followed_id == Post.user_id)).filter(
                 followers.c.follower_id == self.id)
