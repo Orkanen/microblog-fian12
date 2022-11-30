ffac955017f38f9d4dbcaf58320eb41286016e9e
Filip Antonsson <Filip.v.Antonsson@gmail.com>
2022-11-30 10:51:21 +0100

    testing

--- test.sh
+++ test.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
-cd /home/microblog/app
-source ../.venv/bin/activate
+WORKING_DIR="/home/"
+CHILD_A=${CHILD_A:=/home/deps}
+CHILD_B=${CHILD_B:=/home/microblog}
+
+cd $CHILD_A
+. .venv/bin/activate
+cd $CHILD_B
+
 make test
